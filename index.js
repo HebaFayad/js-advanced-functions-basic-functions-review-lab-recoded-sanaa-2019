cb4ec61d1cc677e5841781df738176779583887a
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-11-22 20:10:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// Your code here
+//one
+function saturdayFun(target="roller-skate") {
+  console.log(`This Saturday, I want to ${target}!`);
+}
+saturdayFun();
+//two
 function saturdayFun(target="roller-skate") {
   console.log(`This Saturday, I want to ${target}!`);
 }
